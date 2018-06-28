1acae02fb6643da2cd1a2457fa9f89f680bd81fd
marcuskjerner <marcus@kjerner.se>
2018-06-28 22:59:06 +0200

    Added Count functionality to Aggregators

--- src/App.js
+++ src/App.js
@@ -13,37 +13,36 @@ let fakeServerData = {
       {
         name: 'My Favorites',
         songs: [
-            {name: 'Beat It', duration: 346},
-            {name: 'Cammeloni Makaroni', duration: 221},
-            {name: 'Rosa Helikopter', duration: 303}
-          ]
+          { name: 'Beat It', duration: 346 },
+          { name: 'Cammeloni Makaroni', duration: 221 },
+          { name: 'Rosa Helikopter', duration: 303 }
+        ]
       },
       {
         name: 'Discover Weekly',
-        songs: [{name: 'Le Song', duration: 286}, 
-        {name: 'The Kalsong Song', duration: 286}, 
-        {name: 'Heroes', duration: 286}, 
-        {name: 'One', duration: 286}]
-
+        songs: [
+          { name: 'Le Song', duration: 286 },
+          { name: 'The Kalsong Song', duration: 286 },
+          { name: 'Heroes', duration: 286 },
+          { name: 'One', duration: 286 }]
       },
       {
         name: 'Kids Today',
-        songs: ['Ich bin Schnappi', 'Here comes Pippe Longstocking', 'Donald Ducker']
-      },
-      {
-        name: 'My Best Songs',
-        songs: ['Wunderbaum', '99 Luftballong', 'Scharzwald', 'Kartoffeln']
+        songs: [{ name: 'Ich bin Schnappi', duration: 189 },
+        { name: 'Here comes Pippi Longstocking', duration: 189 },
+        { name: 'Donald Ducker', duration: 189 }
+        ]
       }
     ]
-  } 
+  }
 }
 
 // PlaylistCounter Component
 class PlaylistCounter extends Component {
   render() {
     return (
-      <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
-        <h2>{this.props.playlists && this.props.playlists.length} Playlists</h2>
+      <div style={{ ...defaultStyle, width: '40%', display: 'inline-block' }}>
+        <h2>{this.props.playlists.length} Playlists</h2>
       </div>
     );
   }
@@ -53,9 +52,17 @@ class PlaylistCounter extends Component {
 // HoursCounter Component
 class HoursCounter extends Component {
   render() {
+    // All Songs to an Array
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs);
+    }, []);
+
+    let totalDuration = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration;
+    }, 0);
     return (
-      <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
-        <h2>{this.props.playlists && this.props.playlists.length} Hours</h2>
+      <div style={{ ...defaultStyle, width: '40%', display: 'inline-block' }}>
+        <h2>{Math.round(totalDuration / 60)} Hours</h2>
       </div>
     );
   }
@@ -65,8 +72,8 @@ class HoursCounter extends Component {
 class Filter extends Component {
   render() {
     return (
-      <div style={{defaultStyle}}>
-        <img alt=""/>
+      <div style={{ defaultStyle }}>
+        <img alt="" />
         <input type="text" />
       </div>
     );
@@ -78,8 +85,8 @@ class Filter extends Component {
 class Playlist extends Component {
   render() {
     return (
-      <div style={{...defaultStyle, width: "25%", display: "inline-block"}}>
-        <img alt=""/>
+      <div style={{ ...defaultStyle, width: "25%", display: "inline-block" }}>
+        <img alt="" />
         <h3>Playlist Name</h3>
         <ul>
           <li>Song 1</li>
@@ -93,34 +100,34 @@ class Playlist extends Component {
 
 // MAIN APP
 class App extends Component {
-  
+
   constructor() {
     super();
-    this.state = {serverData: {}};
+    this.state = { serverData: {} };
   }
 
   componentDidMount() {
     // Fake Server Response Time
     setTimeout(() => {
-      this.setState({serverData: fakeServerData});
+      this.setState({ serverData: fakeServerData });
     }, 150);
-    
+
   }
   render() {
     return (
       <div className="App">
         {this.state.serverData.user ?
-          <div> 
+          <div>
             <h1 className="title">
-            {this.state.serverData.user.name}'s Playlist
+              {this.state.serverData.user.name}'s Playlist
           </h1>
-          <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
-          <HoursCounter playlists={this.state.serverData.user.playlists}/>
-          <Filter />
-          <Playlist />
-          <Playlist />
-          <Playlist /> 
-        </div> : <h2 style={defaultStyle}>Loading ...</h2>
+            <PlaylistCounter playlists={this.state.serverData.user.playlists} />
+            <HoursCounter playlists={this.state.serverData.user.playlists} />
+            <Filter />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+          </div> : <h2 style={defaultStyle}>Loading ...</h2>
         }
       </div>
     );
